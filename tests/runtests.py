7596a03e1bc4fc48b03f465415c4e40163df0646
Adam Johnson <me@adamj.eu>
2020-03-17 08:55:08 +0000

    Refs #31370 -- Made runtests.py always use output buffering.

--- tests/runtests.py
+++ tests/runtests.py
@@ -284,7 +284,7 @@ class ActionSelenium(argparse.Action):
 
 def django_tests(verbosity, interactive, failfast, keepdb, reverse,
                  test_labels, debug_sql, parallel, tags, exclude_tags,
-                 test_name_patterns, start_at, start_after, pdb, buffer):
+                 test_name_patterns, start_at, start_after, pdb):
     state = setup(verbosity, test_labels, parallel, start_at, start_after)
     extra_tests = []
 
@@ -305,7 +305,7 @@ def django_tests(verbosity, interactive, failfast, keepdb, reverse,
         exclude_tags=exclude_tags,
         test_name_patterns=test_name_patterns,
         pdb=pdb,
-        buffer=buffer,
+        buffer=True,
     )
     failures = test_runner.run_tests(
         test_labels or get_installed(),
@@ -501,10 +501,6 @@ if __name__ == "__main__":
         '--pdb', action='store_true',
         help='Runs the PDB debugger on error or failure.'
     )
-    parser.add_argument(
-        '-b', '--buffer', action='store_true',
-        help='Discard output of passing tests.',
-    )
     if PY37:
         parser.add_argument(
             '-k', dest='test_name_patterns', action='append',
@@ -571,7 +567,7 @@ if __name__ == "__main__":
             options.debug_sql, options.parallel, options.tags,
             options.exclude_tags,
             getattr(options, 'test_name_patterns', None),
-            options.start_at, options.start_after, options.pdb, options.buffer,
+            options.start_at, options.start_after, options.pdb,
         )
         if failures:
             sys.exit(1)
